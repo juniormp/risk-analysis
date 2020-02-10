33eb5f81ccf7aa68cb909e05fe71cc5a1de573ba
Maurício Junior <junior9771@gmail.com>
2020-02-10 19:36:11 -0300

    add method to retrieve asset rules list on factory

--- domain/factory/risk_analysis_rules_factory.py
+++ domain/factory/risk_analysis_rules_factory.py
@@ -1,9 +1,14 @@
+from domain.entity.rule.asset_rules import AssetRules
 from domain.entity.rule.person_rules import PersonRules
 
 
 class RiskAnalysisRulesFactory:
-    def __init__(self, person_rules: PersonRules):
+    def __init__(self, person_rules: PersonRules, asset_rules: AssetRules):
         self.person_rules = person_rules
+        self.asset_rules = asset_rules
 
     def create_person_rules(self):
         return self.person_rules.get_rules_list
+
+    def create_asset_rules(self):
+        return self.asset_rules.get_rules_list
