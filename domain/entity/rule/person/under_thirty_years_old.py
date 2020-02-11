67adaed1a4c10ae78bc6ffeb89ab5666334bb88b
Maurício Junior <junior9771@gmail.com>
2020-02-11 07:10:54 -0300

    refact under thirty years rule

--- domain/entity/rule/person/under_thirty_years_old.py
+++ domain/entity/rule/person/under_thirty_years_old.py
@@ -1,9 +1,31 @@
-from domain.entity.person import Person
+from domain.entity.risk_analysis import RiskAnalysis
 
 
 class UnderThirtyYearsOld:
-    def execute(self, person=Person):
-        if person.age < 30:
-            return True
+    def execute(self, risk_analysis: RiskAnalysis):
+        person = self.__get_person_from(risk_analysis=risk_analysis)
+        risk_profile = self.__get_risk_profile_from(risk_analysis=risk_analysis)
 
-        return False
+        if person.age < 30:
+            self.__deduct_points_from(risk_profile=risk_profile)
+
+        return risk_analysis
+
+    def __get_person_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.person
+
+    def __get_risk_profile_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.risk_profile
+
+    def __deduct_points_from(self, risk_profile):
+        life_product = risk_profile.risk_score.product['life']
+        life_product.score = -2
+
+        disability = risk_profile.risk_score.product['disability']
+        disability.score = -2
+
+        life_product = risk_profile.risk_score.product['home']
+        life_product.score = -2
+
+        disability = risk_profile.risk_score.product['vehicle']
+        disability.score = -2
--- tests/unit_test/domain/entity/rule/person/test_under_thirty_years_old.py
+++ tests/unit_test/domain/entity/rule/person/test_under_thirty_years_old.py
@@ -1,35 +1,42 @@
 from django.test import TestCase
 from domain.entity.person import Person
+from domain.entity.risk_analysis import RiskAnalysis
 from domain.entity.rule.person.under_thirty_years_old import UnderThirtyYearsOld
+from tests.person_builder import PersonBuilder
+from tests.risk_profile_builder import RiskProfileBuilder
 
 
 class TestUnderThirtyYearsOld(TestCase):
+    def setUp(self):
+        risk_profile_builder = RiskProfileBuilder()
+        self.risk_profile = risk_profile_builder \
+            .with_disability_product() \
+            .with_home_product() \
+            .with_life_product() \
+            .with_vehicle_product() \
+            .build()
+
+        self.person_builder = PersonBuilder()
+        self.rule = UnderThirtyYearsOld()
+
     def test_is_truly_when_person_is_under_thirty_years_old(self):
-        rule = UnderThirtyYearsOld()
-        person = Person(
-            income=None,
-            age=29,
-            dependents=None,
-            marital_status=None,
-            risk_question=None,
-            assets=None
-        )
+        person = self.person_builder.with_age(29).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-        response = rule.execute(person=person)
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        self.assertTrue(response)
+        self.assertEqual(-2, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(-2, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(-2, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(-2, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
 
     def test_is_falsely_when_person_is_over_thirty_years_old(self):
-        rule = UnderThirtyYearsOld()
-        person = Person(
-            income=None,
-            age=30,
-            dependents=None,
-            marital_status=None,
-            risk_question=None,
-            assets=None
-        )
+        person = self.person_builder.with_age(30).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-        response = rule.execute(person=person)
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        self.assertFalse(response)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
