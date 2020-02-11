be190b90a446fbe7acd5a8da1d3965559a46adde
Maurício Junior <junior9771@gmail.com>
2020-02-10 23:19:33 -0300

    refact over sixty years old rule

--- domain/entity/rule/person/over_sixty_years_old.py
+++ domain/entity/rule/person/over_sixty_years_old.py
@@ -1,9 +1,13 @@
-from domain.entity.person import Person
+from domain.entity.risk_analysis import RiskAnalysis
 
 
 class OverSixtyYearsOld:
-    def execute(self, person=Person):
+    def execute(self, risk_analysis: RiskAnalysis):
+        person = self.__get_person_from(risk_analysis=risk_analysis)
         if person.age > 60:
             return True
 
         return False
+
+    def __get_person_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.person
--- tests/unit_test/domain/entity/rule/person/test_over_sixty_years_old.py
+++ tests/unit_test/domain/entity/rule/person/test_over_sixty_years_old.py
@@ -1,9 +1,31 @@
 from django.test import TestCase
 from domain.entity.person import Person
+from domain.entity.product.disability_product import DisabilityProduct
+from domain.entity.product.home_product import HomeProduct
+from domain.entity.product.life_product import LifeProduct
+from domain.entity.product.vehicle_product import VehicleProduct
+from domain.entity.risk_analysis import RiskAnalysis
+from domain.entity.risk_profile import RiskProfile
+from domain.entity.risk_score import RiskScore
 from domain.entity.rule.person.over_sixty_years_old import OverSixtyYearsOld
 
 
 class TestOverSixtyYearsOld(TestCase):
+    def setUp(self):
+        self.home_product = HomeProduct()
+        self.vehicle_product = VehicleProduct()
+        self.life_product = LifeProduct()
+        self.disability_product = DisabilityProduct()
+        self.risk_score = RiskScore(
+            product={
+                'vehicle': self.vehicle_product, 'home': self.home_product,
+                'life': self.life_product, 'disability': self.disability_product
+            }
+        )
+        self.risk_profile = RiskProfile(
+            risk_score=self.risk_score
+        )
+
     def test_is_truly_when_person_is_over_sixty_years_old(self):
         rule = OverSixtyYearsOld()
         person = Person(
@@ -14,8 +36,12 @@ class TestOverSixtyYearsOld(TestCase):
             risk_question=None,
             assets=None
         )
+        risk_analysis = RiskAnalysis(
+            person=person,
+            risk_profile=self.risk_profile
+        )
 
-        response = rule.execute(person=person)
+        response = rule.execute(risk_analysis=risk_analysis)
 
         self.assertTrue(response)
 
@@ -29,7 +55,11 @@ class TestOverSixtyYearsOld(TestCase):
             risk_question=None,
             assets=None
         )
+        risk_analysis = RiskAnalysis(
+            person=person,
+            risk_profile=self.risk_profile
+        )
 
-        response = rule.execute(person=person)
+        response = rule.execute(risk_analysis=risk_analysis)
 
         self.assertFalse(response)
