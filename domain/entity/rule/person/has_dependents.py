194b9935834b20920ccc77ad15939a1834944797
Maurício Junior <junior9771@gmail.com>
2020-02-11 08:23:22 -0300

    refact has dependents rule

--- domain/entity/rule/person/has_dependents.py
+++ domain/entity/rule/person/has_dependents.py
@@ -1,9 +1,26 @@
-from domain.entity.person import Person
+from domain.entity.risk_analysis import RiskAnalysis
+from domain.entity.risk_profile import RiskProfile
 
 
 class HasDependents:
-    def execute(self, person=Person):
-        if person.dependents > 0:
-            return True
+    def execute(self, risk_analysis: RiskAnalysis):
+        person = self.__get_person_from(risk_analysis=risk_analysis)
+        risk_profile = self.__get_risk_profile_from(risk_analysis=risk_analysis)
 
-        return False
+        if person.dependents > 0:
+            self.__add_points_to(risk_profile=risk_profile)
+
+        return risk_analysis
+
+    def __get_person_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.person
+
+    def __get_risk_profile_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.risk_profile
+
+    def __add_points_to(self, risk_profile: RiskProfile):
+        life_product = risk_profile.risk_score.product['disability']
+        life_product.score = +1
+
+        life_product = risk_profile.risk_score.product['life']
+        life_product.score = +1
--- tests/unit_test/domain/entity/rule/person/test_has_dependents.py
+++ tests/unit_test/domain/entity/rule/person/test_has_dependents.py
@@ -1,35 +1,41 @@
 from django.test import TestCase
-from domain.entity.person import Person
+from domain.entity.risk_analysis import RiskAnalysis
 from domain.entity.rule.person.has_dependents import HasDependents
+from tests.person_builder import PersonBuilder
+from tests.risk_profile_builder import RiskProfileBuilder
 
 
 class TestHasDependents(TestCase):
-    def test_is_truly_when_person_has_dependents(self):
-        rule = HasDependents()
-        person = Person(
-            income=None,
-            age=None,
-            dependents=1,
-            marital_status=None,
-            risk_question=None,
-            assets=None
-        )
+    def setUp(self):
+        risk_profile_builder = RiskProfileBuilder()
+        self.risk_profile = risk_profile_builder \
+            .with_disability_product() \
+            .with_home_product() \
+            .with_life_product() \
+            .with_vehicle_product() \
+            .build()
 
-        response = rule.execute(person=person)
+        self.person_builder = PersonBuilder()
+        self.rule = HasDependents()
 
-        self.assertTrue(response)
+    def test_product_score_is_add_in_1_when_person_has_dependents(self):
+        person = self.person_builder.with_dependents(1).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-    def test_is_falsely_when_person_has_no_dependents(self):
-        rule = HasDependents()
-        person = Person(
-            income=None,
-            age=None,
-            dependents=0,
-            marital_status=None,
-            risk_question=None,
-            assets=None
-        )
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        response = rule.execute(person=person)
+        self.assertEqual(+1, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(+1, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
 
-        self.assertFalse(response)
+    def test_product_score_is_not_add_in_1_when_person_has_not_dependents(self):
+        person = self.person_builder.with_dependents(0).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
+
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
+
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
