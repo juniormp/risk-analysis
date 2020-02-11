c0cb835f6b8f8c6f6de01ae1a1e4876eda20c8a2
Maurício Junior <junior9771@gmail.com>
2020-02-10 22:56:46 -0300

    refact - change status to default

--- domain/entity/product/vehicle_product.py
+++ domain/entity/product/vehicle_product.py
@@ -1,9 +1,9 @@
-from domain.entity.product.product import Product, PRODUCT_SCORE
+from domain.entity.product.product import Product, PRODUCT_SCORE_DEFAULT
 
 
 class VehicleProduct(Product):
     def __init__(self):
-        self.score = PRODUCT_SCORE[0]
+        self.score = PRODUCT_SCORE_DEFAULT
 
     def __eq__(self, other):
         return isinstance(other, VehicleProduct) and \
