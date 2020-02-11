c5d308580dac473dcc2b0490c4dce79a49f0b571
Maurício Junior <junior9771@gmail.com>
2020-02-11 08:42:06 -0300

    refact vehicle produced last five years rule

--- domain/entity/rule/asset/is_vehicle_produced_last_five_years.py
+++ domain/entity/rule/asset/is_vehicle_produced_last_five_years.py
@@ -1,12 +1,28 @@
+from domain.entity.risk_analysis import RiskAnalysis
 from domain.entity.vehicle import Vehicle
 import pendulum
 
 
 class VehicleProducedLastFiveYears:
-    def execute(self, vehicle=Vehicle):
+    def execute(self, risk_analysis: RiskAnalysis):
+        person = self.__get_person_from(risk_analysis=risk_analysis)
+        risk_profile = self.__get_risk_profile_from(risk_analysis=risk_analysis)
         time_right_now = pendulum.now()
         five_years_ago = time_right_now.subtract(years=5)
-        if vehicle.year_manufactured.year > five_years_ago.year:
-            return True
 
-        return False
+        for asset in person.assets:
+            if type(asset) == Vehicle:
+                if asset.year_manufactured.year > five_years_ago.year:
+                    self.__add_points_to(risk_profile)
+
+        return risk_analysis
+
+    def __get_person_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.person
+
+    def __get_risk_profile_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.risk_profile
+
+    def __add_points_to(self, risk_profile):
+        vehicle = risk_profile.risk_score.product['vehicle']
+        vehicle.score = +1
--- tests/unit_test/domain/entity/rule/asset/test_vehicle_produced_last_five_years.py
+++ tests/unit_test/domain/entity/rule/asset/test_vehicle_produced_last_five_years.py
@@ -1,29 +1,49 @@
 from django.test import TestCase
+from domain.entity.risk_analysis import RiskAnalysis
 from domain.entity.rule.asset.is_vehicle_produced_last_five_years import VehicleProducedLastFiveYears
 from domain.entity.vehicle import Vehicle
 from freezegun import freeze_time
+from tests.person_builder import PersonBuilder
+from tests.risk_profile_builder import RiskProfileBuilder
 import datetime
 
 
 class TestVehicleProducedLastFiveYears(TestCase):
+    def setUp(self):
+        risk_profile_builder = RiskProfileBuilder()
+        self.risk_profile = risk_profile_builder \
+            .with_disability_product() \
+            .with_home_product() \
+            .with_life_product() \
+            .with_vehicle_product() \
+            .build()
+
+        self.person_builder = PersonBuilder()
+        self.rule = VehicleProducedLastFiveYears()
+
     @freeze_time("2020-01-01")
-    def test_is_truly_when_vehicle_was_produced_last_five_years(self):
-        rule = VehicleProducedLastFiveYears()
-        vehicle = Vehicle(
-            year_manufactured=datetime.datetime(2015, 1, 1)
-        )
+    def test_product_score_is_add_in_1_when_vehicle_was_produced_last_5_years(self):
+        vehicle = Vehicle(year_manufactured=datetime.datetime(2015, 1, 1))
+        person = self.person_builder.with_asset(asset=vehicle).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-        response = rule.execute(vehicle=vehicle)
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        self.assertTrue(response)
+        self.assertEqual(+1, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['life'].score)
 
     @freeze_time("2020-01-01")
-    def test_is_falsely_when_vehicle_was_not_produced_last_five_years(self):
-        rule = VehicleProducedLastFiveYears()
-        vehicle = Vehicle(
-            year_manufactured=datetime.datetime(2013, 12, 31)
-        )
+    def test_product_score_is_not_add_in_1_when_vehicle_was_produced_last_5_years(self):
+        vehicle = Vehicle(year_manufactured=datetime.datetime(2013, 12, 31))
+        self.person_builder.person.assets = []
+        person = self.person_builder.with_asset(asset=vehicle).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-        response = rule.execute(vehicle=vehicle)
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        self.assertFalse(response)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['life'].score)
