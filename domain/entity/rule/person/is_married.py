248e5fd0ec9dd08c74d3b4f61c9bb8f5a53ef33c
Maurício Junior <junior9771@gmail.com>
2020-02-11 08:33:02 -0300

    refact is married rule

--- domain/entity/rule/person/is_married.py
+++ domain/entity/rule/person/is_married.py
@@ -1,9 +1,27 @@
-from domain.entity.person import Person, MARITAL_STATUS_MARRIED
+from domain.entity.person import MARITAL_STATUS_MARRIED
+from domain.entity.risk_analysis import RiskAnalysis
+from domain.entity.risk_profile import RiskProfile
 
 
 class IsMarried:
-    def execute(self, person=Person):
-        if person.marital_status == MARITAL_STATUS_MARRIED:
-            return True
+    def execute(self, risk_analysis: RiskAnalysis):
+        person = self.__get_person_from(risk_analysis=risk_analysis)
+        risk_profile = self.__get_risk_profile_from(risk_analysis=risk_analysis)
 
-        return False
+        if person.marital_status == MARITAL_STATUS_MARRIED:
+            self.__add_and_deduct_points_from(risk_profile=risk_profile)
+
+        return risk_analysis
+
+    def __get_person_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.person
+
+    def __get_risk_profile_from(self, risk_analysis: RiskAnalysis):
+        return risk_analysis.risk_profile
+
+    def __add_and_deduct_points_from(self, risk_profile: RiskProfile):
+        life_product = risk_profile.risk_score.product['life']
+        life_product.score = +1
+
+        life_product = risk_profile.risk_score.product['disability']
+        life_product.score = -1
--- tests/unit_test/domain/entity/rule/person/test_is_married.py
+++ tests/unit_test/domain/entity/rule/person/test_is_married.py
@@ -1,35 +1,42 @@
 from django.test import TestCase
-from domain.entity.person import Person, MARITAL_STATUS_SINGLE, MARITAL_STATUS_MARRIED
+from domain.entity.person import MARITAL_STATUS_SINGLE, MARITAL_STATUS_MARRIED
+from domain.entity.risk_analysis import RiskAnalysis
 from domain.entity.rule.person.is_married import IsMarried
+from tests.person_builder import PersonBuilder
+from tests.risk_profile_builder import RiskProfileBuilder
 
 
 class TestIsMarried(TestCase):
-    def test_is_truly_when_person_is_married(self):
-        rule = IsMarried()
-        person = Person(
-            income=None,
-            age=None,
-            dependents=None,
-            marital_status=MARITAL_STATUS_MARRIED,
-            risk_question=None,
-            assets=None
-        )
+    def setUp(self):
+        risk_profile_builder = RiskProfileBuilder()
+        self.risk_profile = risk_profile_builder \
+            .with_disability_product() \
+            .with_home_product() \
+            .with_life_product() \
+            .with_vehicle_product() \
+            .build()
 
-        response = rule.execute(person=person)
+        self.person_builder = PersonBuilder()
+        self.rule = IsMarried()
 
-        self.assertTrue(response)
+    def test_product_score_is_add_in_1_and_deduct_in_1_when_person_is_married(self):
+        person = self.person_builder.with_marital_status(MARITAL_STATUS_MARRIED).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
 
-    def test_is_falsely_when_person_has_no_income(self):
-        rule = IsMarried()
-        person = Person(
-            income=None,
-            age=None,
-            dependents=None,
-            marital_status=MARITAL_STATUS_SINGLE,
-            risk_question=None,
-            assets=None
-        )
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
 
-        response = rule.execute(person=person)
+        self.assertEqual(+1, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(-1, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
 
-        self.assertFalse(response)
+    def test_product_score_is_not_add_in_1_and_not_deduct_in_1_when_person_is_single(self):
+        person = self.person_builder.with_marital_status(MARITAL_STATUS_SINGLE).build()
+        risk_analysis = RiskAnalysis(person=person, risk_profile=self.risk_profile)
+
+        risk_analysis = self.rule.execute(risk_analysis=risk_analysis)
+
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['life'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['disability'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['home'].score)
+        self.assertEqual(0, risk_analysis.risk_profile.risk_score.product['vehicle'].score)
