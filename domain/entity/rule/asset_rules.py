836308d1f3e113d12ca52bfa0689a3fa6abd7477
Maurício Junior <junior9771@gmail.com>
2020-02-11 22:20:18 -0300

    refact asset and person rules

--- domain/entity/rule/asset_rules.py
+++ domain/entity/rule/asset_rules.py
@@ -1,13 +1,10 @@
+from domain.entity.rule.asset.is_house_mortgaged import IsHouseMortgaged
+from domain.entity.rule.asset.is_vehicle_produced_last_five_years import VehicleProducedLastFiveYears
+
+
 class AssetRules:
-    is_house_mortgaged = None
-    is_vehicle_produced_las_five_years = None
-
-    def __init__(self, is_house_mortgaged, is_vehicle_produced_las_five_years):
-        self.is_house_mortgaged = is_house_mortgaged
-        self.is_vehicle_produced_las_five_years = is_vehicle_produced_las_five_years
-
     def get_rules_list(self):
         return [
-            self.is_house_mortgaged,
-            self.is_vehicle_produced_las_five_years
+            IsHouseMortgaged(),
+            VehicleProducedLastFiveYears()
         ]
--- domain/entity/rule/person_rules.py
+++ domain/entity/rule/person_rules.py
@@ -10,35 +10,15 @@ from domain.entity.rule.person.under_thirty_years_old import UnderThirtyYearsOld
 
 
 class PersonRules:
-    def __init__(self,
-                 between_thirty_and_forty_years_old: BetweenThirtyAndFortyYearsOld,
-                 has_dependents: HasDependents,
-                 has_house: HasHouse,
-                 has_income: HasIncome,
-                 has_income_above_two_hundred: HasIncomeAboveTwoHundred,
-                 has_vehicle: HasVehicle,
-                 is_married: IsMarried,
-                 over_sixty_years_old: IsOverSixtyYearsOld,
-                 under_thirty_years_old: UnderThirtyYearsOld):
-        self.between_thirty_and_forty_years_old = between_thirty_and_forty_years_old
-        self.has_dependents = has_dependents
-        self.has_house = has_house
-        self.has_income = has_income
-        self.has_income_above_two_hundred = has_income_above_two_hundred
-        self.has_vehicle = has_vehicle
-        self.is_married = is_married
-        self.over_sixty_years_old = over_sixty_years_old
-        self.under_thirty_years_old = under_thirty_years_old
-
     def get_rules_list(self):
         return [
-            self.between_thirty_and_forty_years_old,
-            self.has_dependents,
-            self.has_house,
-            self.has_income,
-            self.has_income_above_two_hundred,
-            self.has_vehicle,
-            self.is_married,
-            self.over_sixty_years_old,
-            self.under_thirty_years_old
+            BetweenThirtyAndFortyYearsOld(),
+            HasDependents(),
+            HasHouse(),
+            HasIncome(),
+            HasIncomeAboveTwoHundred(),
+            HasVehicle(),
+            IsMarried(),
+            IsOverSixtyYearsOld(),
+            UnderThirtyYearsOld()
         ]
