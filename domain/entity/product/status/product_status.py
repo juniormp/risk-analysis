6cf71ad50ff4d676810540370876acf17a41b9a0
Maurício Junior <junior9771@gmail.com>
2020-02-12 00:42:09 -0300

    create product status classes

---
+++ domain/entity/product/status/product_status.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+
+PRODUCT_STATUS_ECONOMIC = 'economic'
+PRODUCT_STATUS_REGULAR = 'regular'
+PRODUCT_STATUS_RESPONSIBLE = 'responsible'
+
+
+class ProductStatus(ABC):
+    name: str
+
+    @abstractmethod
+    def set_condition(self, score: int):
+        pass
---
+++ domain/entity/product/status/regular_status.py
@@ -0,0 +1,9 @@
+from domain.entity.product.status.product_status import ProductStatus, PRODUCT_STATUS_REGULAR
+
+
+class RegularStatus(ProductStatus):
+    def __init__(self):
+        self.name = PRODUCT_STATUS_REGULAR
+
+    def set_condition(self, score: int):
+        return [score == 1 and score == 2]
