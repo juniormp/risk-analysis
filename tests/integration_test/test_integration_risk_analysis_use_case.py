291e52ca004ba12d434f3a28dd2a1713013d2c78
Maurício Junior <junior9771@gmail.com>
2020-02-12 22:38:45 -0300

    add integration test cenario

--- tests/integration_test/test_integration_risk_analysis_use_case.py
+++ tests/integration_test/test_integration_risk_analysis_use_case.py
@@ -22,18 +22,51 @@ class TestIntegrationRiskAnalysisUseCase(TestCase):
                                                      risk_analysis_rules_factory=ServiceRegistry.factory_risk_analysis_rules(),
                                                      risk_analysis_factory=ServiceRegistry.factory_risk_analysis())
 
-        risk_analysis = risk_analysis_use_case.execute(user_information=user_information)
+        risk_profile = risk_analysis_use_case.execute(user_information=user_information)
 
-        self.assertEqual(-2, risk_analysis.risk_profile.get_product_by(HOME_PRODUCT).get_score())
-        self.assertEqual(PRODUCT_SCORE_ECONOMIC, risk_analysis.risk_profile.get_product_by(HOME_PRODUCT).get_status())
+        self.assertEqual(0, risk_profile.get_product_by(HOME_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC, risk_profile.get_product_by(HOME_PRODUCT).get_status())
 
-        self.assertEqual(-2, risk_analysis.risk_profile.get_product_by(VEHICLE_PRODUCT).get_score())
+        self.assertEqual(1, risk_profile.get_product_by(VEHICLE_PRODUCT).get_score())
         self.assertEqual(PRODUCT_SCORE_ECONOMIC,
-                         risk_analysis.risk_profile.get_product_by(VEHICLE_PRODUCT).get_status())
+                         risk_profile.get_product_by(VEHICLE_PRODUCT).get_status())
 
-        self.assertEqual(-1, risk_analysis.risk_profile.get_product_by(LIFE_PRODUCT).get_score())
-        self.assertEqual(PRODUCT_SCORE_ECONOMIC, risk_analysis.risk_profile.get_product_by(LIFE_PRODUCT).get_status())
+        self.assertEqual(2, risk_profile.get_product_by(LIFE_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC, risk_profile.get_product_by(LIFE_PRODUCT).get_status())
 
-        self.assertEqual(-2, risk_analysis.risk_profile.get_product_by(DISABILITY_PRODUCT).get_score())
+        self.assertEqual(0, risk_profile.get_product_by(DISABILITY_PRODUCT).get_score())
         self.assertEqual(PRODUCT_SCORE_ECONOMIC,
-                         risk_analysis.risk_profile.get_product_by(DISABILITY_PRODUCT).get_status())
+                         risk_profile.get_product_by(DISABILITY_PRODUCT).get_status())
+
+    def test_should_execute_risk_analysis_first_simulation_two(self):
+        user_information = {
+            "age": 35,
+            "dependents": 2,
+            "house": {"ownership_status": "owned"},
+            "income": 300,
+            "marital_status": "married",
+            "risk_question": [0, 1, 1],
+            "vehicle": {"year": "2018"}
+        }
+
+        risk_analysis_use_case = RiskAnalysisUseCase(risk_analysis_service=ServiceRegistry.service_risk_analysis(),
+                                                     risk_analysis_rules_factory=ServiceRegistry.factory_risk_analysis_rules(),
+                                                     risk_analysis_factory=ServiceRegistry.factory_risk_analysis())
+
+        risk_profile = risk_analysis_use_case.execute(user_information=user_information)
+
+        self.assertEqual(-2, risk_profile.get_product_by(HOME_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC,
+                         risk_profile.get_product_by(HOME_PRODUCT).get_status())
+
+        self.assertEqual(-1, risk_profile.get_product_by(VEHICLE_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC,
+                         risk_profile.get_product_by(VEHICLE_PRODUCT).get_status())
+
+        self.assertEqual(0, risk_profile.get_product_by(LIFE_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC,
+                         risk_profile.get_product_by(LIFE_PRODUCT).get_status())
+
+        self.assertEqual(-2, risk_profile.get_product_by(DISABILITY_PRODUCT).get_score())
+        self.assertEqual(PRODUCT_SCORE_ECONOMIC,
+                         risk_profile.get_product_by(DISABILITY_PRODUCT).get_status())
